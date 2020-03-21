d4c9d2cac2f39598f19cbbc374a38ea33f0f49c7
KimCaves <55900124+KimCaves@users.noreply.github.com>
2020-03-21 14:52:45 -0600

    postgresq

--- src/main/java/com/uatx/inventarios/service/ProductoService.java
+++ src/main/java/com/uatx/inventarios/service/ProductoService.java
@@ -1,4 +1,9 @@
 package com.uatx.inventarios.service;
 
+import com.uatx.inventarios.dto.ProductoDTO;
+
 public interface ProductoService {
+
+    Long store (ProductoDTO productoDTO);
+
 }
--- src/main/java/com/uatx/inventarios/service/impl/ProductoServiceImpl.java
+++ src/main/java/com/uatx/inventarios/service/impl/ProductoServiceImpl.java
@@ -1,4 +1,30 @@
 package com.uatx.inventarios.service.impl;
 
 public class ProductoServiceImpl {
-}
+import com.uatx.inventarios.dto.ProductoDTO;
+import com.uatx.inventarios.model.Producto;
+import com.uatx.inventarios.repository.ProductoRepository;
+import com.uatx.inventarios.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+
+
+    @Service
+    public class ProductoServiceImpl implements ProductoService {
+
+        @Autowired
+        private ProductoRepository productoRepository;
+
+        @Override
+        public Long store(ProductoDTO productoDTO) {
+            Producto producto = new Producto();
+            producto.setNombre(productoDTO.getNombre());
+
+            productoRepository.save(producto);
+
+            return producto.getId();
+        }
+    }
